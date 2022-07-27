a16c9dcd968e86913bbd8e8a83ec880d78205ebc
DESKTOP-1L9PO9A\crypt <alsehddjs@gmail.com>
2022-07-27 15:03:24 +0900

    save to localstorage

--- todolist/script.js
+++ todolist/script.js
@@ -2,14 +2,22 @@ const todoForm = document.querySelector("#todoForm");
 const input = todoForm.querySelector("input");
 const ul = document.querySelector("#list ul");
 
-function addTodo(inputVal) {
+const TODOS_KEY = "todos";
+let toDos = [];
+
+function saveToDos() {
+  localStorage.setItem(TODOS_KEY, JSON.stringify(toDos));
+}
+
+function addTodo(inputObj) {
   const li = document.createElement("li");
+  li.id = inputObj.id;
   const span = document.createElement("span");
   const addBtn = document.createElement("button");
   ul.appendChild(li);
   li.appendChild(span);
   li.appendChild(addBtn);
-  span.innerText = inputVal;
+  span.innerText = inputObj.text;
   addBtn.innerText = "X";
   addBtn.addEventListener("click", removeTodo);
 }
@@ -19,12 +27,29 @@ function removeTodo(e) {
   //   ul.removeChild(e.target.parentElement);
   const li = e.target.parentElement;
   li.remove();
+  toDos = toDos.filter((toDo) => toDo.id !== parseInt(li.id));
+  saveToDos();
 }
 
-// form 기본설정 해제, addTodo() 실행?
+// form에서 submit 할 시 실행될 것들
+// (맨 처음에 실행되는 것)
 todoForm.addEventListener("submit", (e) => {
   e.preventDefault();
   const inputVal = input.value;
   input.value = "";
-  addTodo(inputVal);
+  const inputObj = {
+    text: inputVal,
+    id: Date.now(),
+  };
+  toDos.push(inputObj);
+  addTodo(inputObj);
+  saveToDos();
 });
+
+const savedToDos = localStorage.getItem(TODOS_KEY);
+
+if (savedToDos !== null) {
+  const parseToDos = JSON.parse(savedToDos);
+  toDos = parseToDos;
+  parseToDos.forEach(addTodo);
+}
